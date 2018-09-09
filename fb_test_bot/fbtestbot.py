d473ca253573a23a994c50e0e41e26b12566bd0b
Tanuj Jain <tanuj.jain@idealo.de>
2018-09-10 00:00:00 +0200

    Got echo bot working lcoally with ngrok.

--- fb_test_bot/fbtestbot.py
+++ fb_test_bot/fbtestbot.py
@@ -1,15 +1,16 @@
-from flask import Flask, request
 import requests
 import sys
 import os
 import json
-from bs4 import BeautifulSoup
 import io
-from PIL import Image
 import datetime
+from bs4 import BeautifulSoup
+from flask import Flask, request
+from PIL import Image
+
 #from src.fb_test_bot.Credentials import *
-VERIFY_TOKEN = os.environ.get('VERIFY_TOKEN')
-PAGE_ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN')
+VERIFY_TOKEN = 'anything_to_verify' #os.environ.get('VERIFY_TOKEN')
+PAGE_ACCESS_TOKEN = 'EAAeUO39QC9UBAGDEA22STLEyqFmZASY3w2wzbklZCu4fcxeW3Qko8wLXddKlMwjRiLBydGWUFZCl2JolOVTvvMxJY8xn99l0mZCWXkDDFQOW1SDjb9ZAAiBSOiXnLxXq4mBfJbBwm56IkjqDhVKbh4Q3zuwcYjwEBVb6tZBZA9lBQZDZD' #os.environ.get('PAGE_ACCESS_TOKEN')
 
 app = Flask(__name__)
 
@@ -24,18 +25,23 @@ def dilbert():
     image_returned = Image.open(io.BytesIO(req_dilbert.content))
     return image_returned
 
+
 @app.route('/', methods=['GET'])
 def handle_verification():
+    print('In root endpoint with token: %s'%request.args.get('hub.verify_token', ''))
+    print(request.args.get('hub.verify_token', '') == VERIFY_TOKEN)
     if request.args.get('hub.verify_token', '') == VERIFY_TOKEN:
+        print('YEAH!')
         return request.args.get('hub.challenge', 200)
     else:
-        return 'Error, wrong validation token'
+        return 'Errorrrrr, wrong validation token'
 
 
 @app.route('/', methods=['POST'])
 def handle_messages():
     data = request.get_json()
     log(data)
+    print('In post with data %s'%data)
 
     if data["object"] == "page":
 
@@ -46,8 +52,9 @@ def handle_messages():
                     sender_id = messaging_event["sender"]["id"]
                     recipient_id = messaging_event["recipient"]["id"]
                     message_text = messaging_event["message"]["text"]
-                    extra_string = 'blaaa'
-                    send_message(sender_id, message_text + extra_string)
+                    #extra_string = 'blaaa'
+                    #send_message(sender_id, message_text + extra_string)
+                    send_message_echo(sender_id, message_text)
 
                 if messaging_event.get("delivery"):
                     pass
@@ -59,6 +66,39 @@ def handle_messages():
                     pass
 
     return "ok", 200
+
+
+def send_message_echo(recipient_id, message_text):
+    """Send the message text to recipient with id recipient.
+      """
+    print('In echo function!')
+    '''r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+                      params={"access_token": PAGE_ACCESS_TOKEN},
+                      data=json.dumps({
+                          "recipient": {"id": recipient_id},
+                          "message": {"text": message_text}
+                      }),
+                      headers={'Content-type': 'application/json'})'''
+
+    params = {
+        "access_token": PAGE_ACCESS_TOKEN
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != requests.codes.ok:
+        print(r)
+        print(r.text)
 
 
 def send_message(recipient_id, message_text):
@@ -91,4 +131,7 @@ def log(message):  # simple wrapper for logging to stdout on heroku
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    print(VERIFY_TOKEN)
+    print(PAGE_ACCESS_TOKEN)
+    #app.run(host='127.0.0.1',debug=True, port=port)
+    app.run(host='127.0.0.1', port=port)
