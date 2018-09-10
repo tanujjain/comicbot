b019e1482ead930b091e8190440e306ff1174691
Tanuj Jain <tanuj.jain@idealo.de>
2018-09-10 23:18:51 +0200

    Added image return function url.

--- fb_test_bot/fbtestbot.py
+++ fb_test_bot/fbtestbot.py
@@ -21,9 +21,10 @@ def dilbert():
     page = requests.get(today_url)
     soup = BeautifulSoup(page.content, 'html.parser')
     url_img = soup.find_all('div', {'class': ['img-comic-container']})[0].find('img')['src']
-    req_dilbert = requests.get(url_img)
-    image_returned = Image.open(io.BytesIO(req_dilbert.content))
-    return image_returned
+    return url_img
+    #req_dilbert = requests.get(url_img)
+    #image_returned = Image.open(io.BytesIO(req_dilbert.content))
+    #return image_returned
 
 
 @app.route('/', methods=['GET'])
@@ -52,9 +53,10 @@ def handle_messages():
                     sender_id = messaging_event["sender"]["id"]
                     recipient_id = messaging_event["recipient"]["id"]
                     message_text = messaging_event["message"]["text"]
-                    #extra_string = 'blaaa'
-                    #send_message(sender_id, message_text + extra_string)
                     send_message_echo(sender_id, message_text)
+
+                    if message_text == 'dilbert':
+                        send_comic(sender_id, dilbert())
 
                 if messaging_event.get("delivery"):
                     pass
@@ -117,6 +119,36 @@ def send_message(recipient_id, message_text):
         "message": {
             "text": message_text
         }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+    log(r.text)
+
+
+def send_comic(recipient_id, img_url):
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=img_url))
+
+    #with open('dilbert_image.jpg', 'rb') as f:
+        #my_img = f.read()
+
+    params = {
+        "access_token": PAGE_ACCESS_TOKEN
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "attachment": {
+                "type": "image",
+                "payload": {"url": img_url}
+            }
+        }#,
+        #"filedata": (os.path.basename('dilbert_image.jpg'), open('dilbert_image.jpg', 'rb')) # 'image/jpg'
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
