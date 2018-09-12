d206f274d41219b23a780581bb497d836d83cdd3
Tanuj Jain <tanuj.jain@idealo.de>
2018-09-12 20:11:57 +0200

    Commented out some lines.

--- fb_test_bot/fbtestbot.py
+++ fb_test_bot/fbtestbot.py
@@ -55,7 +55,7 @@ def handle_verification():
 def handle_messages():
     data = request.get_json()
     log(data)
-    print('In post with data %s'%data)
+    #print('In post with data %s'%data)
 
     if data["object"] == "page":
 
@@ -92,7 +92,7 @@ def handle_messages():
 def send_message_echo(recipient_id, message_text):
     """Send the message text to recipient with id recipient.
       """
-    print('In echo function!')
+    #print('In echo function!')
     '''r = requests.post("https://graph.facebook.com/v2.6/me/messages",
                       params={"access_token": PAGE_ACCESS_TOKEN},
                       data=json.dumps({
@@ -122,27 +122,27 @@ def send_message_echo(recipient_id, message_text):
         print(r.text)
 
 
-def send_message(recipient_id, message_text):
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-
-    params = {
-        "access_token": PAGE_ACCESS_TOKEN
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-    log(r.text)
+# def send_message(recipient_id, message_text):
+#     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+#
+#     params = {
+#         "access_token": PAGE_ACCESS_TOKEN
+#     }
+#     headers = {
+#         "Content-Type": "application/json"
+#     }
+#     data = json.dumps({
+#         "recipient": {
+#             "id": recipient_id
+#         },
+#         "message": {
+#             "text": message_text
+#         }
+#     })
+#     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+#     if r.status_code != 200:
+#         log(r.status_code)
+#     log(r.text)
 
 
 def send_comic(recipient_id, img_url):
