9514fca1a980d389421d495b616d9f8c01187ab9
Tanuj Jain <tanuj.jain@idealo.de>
2018-09-16 23:34:10 +0200

    Cleaned the code.

--- fb_test_bot/fbtestbot.py
+++ fb_test_bot/fbtestbot.py
@@ -22,9 +22,6 @@ def dilbert():
     soup = BeautifulSoup(page.content, 'html.parser')
     url_img = soup.find_all('div', {'class': ['img-comic-container']})[0].find('img')['src']
     return url_img
-    #req_dilbert = requests.get(url_img)
-    #image_returned = Image.open(io.BytesIO(req_dilbert.content))
-    #return image_returned
 
 
 def xkcd():
@@ -64,7 +61,6 @@ def handle_verification():
 def handle_messages():
     data = request.get_json()
     log(data)
-    #print('In post with data %s'%data)
 
     if data["object"] == "page":
 
@@ -104,7 +100,6 @@ def handle_messages():
 def send_message_echo(recipient_id, message_text):
     """Send the message text to recipient with id recipient.
       """
-    #print('In echo function!')
     '''r = requests.post("https://graph.facebook.com/v2.6/me/messages",
                       params={"access_token": PAGE_ACCESS_TOKEN},
                       data=json.dumps({
@@ -134,34 +129,8 @@ def send_message_echo(recipient_id, message_text):
         print(r.text)
 
 
-# def send_message(recipient_id, message_text):
-#     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-#
-#     params = {
-#         "access_token": PAGE_ACCESS_TOKEN
-#     }
-#     headers = {
-#         "Content-Type": "application/json"
-#     }
-#     data = json.dumps({
-#         "recipient": {
-#             "id": recipient_id
-#         },
-#         "message": {
-#             "text": message_text
-#         }
-#     })
-#     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-#     if r.status_code != 200:
-#         log(r.status_code)
-#     log(r.text)
-
-
 def send_comic(recipient_id, img_url):
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=img_url))
-
-    #with open('dilbert_image.jpg', 'rb') as f:
-        #my_img = f.read()
 
     params = {
         "access_token": PAGE_ACCESS_TOKEN
@@ -178,8 +147,7 @@ def send_comic(recipient_id, img_url):
                 "type": "image",
                 "payload": {"url": img_url}
             }
-        }#,
-        #"filedata": (os.path.basename('dilbert_image.jpg'), open('dilbert_image.jpg', 'rb')) # 'image/jpg'
+        }
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
@@ -196,5 +164,4 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     print(VERIFY_TOKEN)
     print(PAGE_ACCESS_TOKEN)
-    #app.run(host='127.0.0.1',debug=True, port=port)
     app.run(host='0.0.0.0', port=port)
